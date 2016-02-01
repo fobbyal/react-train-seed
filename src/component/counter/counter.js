83e6a1283c9c52127003a3d3b88da5c0c9ab0f23
Alex Liang <aliang@integconsulting.com>
2016-02-01 01:52:57 -0500

    made counter completely dump component

--- src/component/counter/counter.js
+++ src/component/counter/counter.js
@@ -2,25 +2,21 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as CounterAction from '../../action/counter-action';
 
-export const Counter = ({ increase, decrease, count, index }) => {
+export const Counter = ({ increase, decrease, count }) => {
   return (
       <div style={{ display: 'inline-block' }}>
         <h1>{ count }</h1>
-        <button onClick={increase(index)}>+</button>
-        <button onClick={decrease(index)}>-</button>
+        <button onClick={increase}>+</button>
+        <button onClick={decrease}>-</button>
       </div>
     );
 };
 
 export default connect(
-  null, (dispatch) => {
+  null, (dispatch, { index }) => {
     return {
-      increase: (index) => {
-        return () => dispatch(CounterAction.increase(index));
-      },
-      decrease: (index) => {
-        return () => dispatch(CounterAction.decrease(index));
-      }
+      increase: () => dispatch(CounterAction.increase(index)),
+      decrease: () => dispatch(CounterAction.decrease(index))
     };
   }
 )(Counter);
