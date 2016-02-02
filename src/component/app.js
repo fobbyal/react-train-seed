68073fb59e5bd15b5223f701eb1b699814cb7df0
Alex Liang <alex.c.liang@gmail.com>
2016-02-02 02:36:33 -0500

    middleware

--- src/component/app.js
+++ src/component/app.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { connect } from 'react-redux';
+import { push } from 'react-router-redux';
 
-export default ({ children }) => (
+export const App = ({ children, goToCounters }) => (
     <div>
       <h1>Welcome to App with Router</h1>
       <ul>
       <li><Link to={'/'}>Todo</Link></li>
       <li><Link to={'/counters'}>Counters</Link></li>
+      <li><button onClick={goToCounters}>Click Me to go to Counters</button></li>
       </ul>
 
       <div>{children}</div>
     </div>
 );
+export default connect(
+  null,
+  (dispatch) => ({
+    goToCounters:() => {dispatch({type:'CRAZY'});}
+  })
+)(App);
--- src/store.js
+++ src/store.js
@@ -4,12 +4,46 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 /** for routing**/
 import { browserHistory } from 'react-router';
 import { syncHistory, routeReducer } from 'react-router-redux';
+import * as CounterActions from './action/counter-action';
+import { push } from 'react-router-redux';
 
 const finalReducer = combineReducers({ todos, counters, routing: routeReducer });
 
-/** middlewares **/
+
+/* *middlewares **/
+const createLogger = ({ getState }) => {
+  return (next) => (action) => {
+    const console = window.console;
+    const prevState = getState();
+    const returnValue = next(action);
+    const nextState = getState();
+    // const actionType = String(action.type);
+    // const message = `action ${actionType}`;
+    console.log(`%c prev state`, `color: #9E9E9E`, prevState);
+    console.log(`%c action`, `color: #03A9F4`, action);
+    console.log(`%c next state`, `color: #4CAF50`, nextState);
+    return returnValue;
+  };
+};
+
+/* *middlewares **/
+const crazy = ({ getState,dispatch }) => {
+  return (next) => (action) => {
+    if (action.type === 'CRAZY') {
+      dispatch(push('/counters'));
+      dispatch(CounterActions.add());
+      dispatch(CounterActions.add());
+      dispatch(CounterActions.add());
+      return next(CounterActions.add());
+    }
+    return next(action);
+  };
+};
+
+
+
 const routerMiddleware = syncHistory(browserHistory);
-const createStoreWithMiddleware = applyMiddleware(routerMiddleware)(createStore);
+const createStoreWithMiddleware = applyMiddleware(routerMiddleware, createLogger, crazy)(createStore);
 
 const store = createStoreWithMiddleware(finalReducer);
 
