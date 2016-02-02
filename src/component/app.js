fed7468b5b75ec36a4a808d268001663f5e1057e
Alex Liang <alex.c.liang@gmail.com>
2016-02-02 01:54:44 -0500

    finished routing

--- src/component/app.js
+++ src/component/app.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router';
 
-export default ({children}) => (
+export default ({ children }) => (
     <div>
-      <h1> Welcome to routed app</h1>
+      <h1>Welcome to App with Router</h1>
+      <ul>
+      <li><Link to={'/'}>Todo</Link></li>
+      <li><Link to={'/counters'}>Counters</Link></li>
+      </ul>
+
       <div>{children}</div>
     </div>
 );
