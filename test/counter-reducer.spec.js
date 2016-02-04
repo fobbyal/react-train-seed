a8dcd74374ab850760528c5245be2784b45cd165
Alex Liang <alex.c.liang@gmail.com>
2016-02-04 02:17:22 -0500

    finished testing train 1

--- test/counter-reducer.spec.js
+++ test/counter-reducer.spec.js
@@ -4,9 +4,20 @@ import * as Actions from '../src/action/counter-action';
 import { expect } from 'chai';
 
 describe('Counter Reducer', () => {
-  let state = counterReducer(undefined, Actions.add());
-  it('Should be [0] after add aciton', () => {
+
+  it('Should have new counter after add aciton', () => {
+    const state = counterReducer(undefined, Actions.add());
     expect(state).to.deep.equal([0]);
+  });
+
+  it('Should have incrased counter at index 0',()=>{
+    const state = counterReducer([0],Actions.increase(0));
+    expect(state).to.deep.equal([1]);
+  });
+
+  it('Should have incrased counter at index 2',()=>{
+    const state = counterReducer([0,1,3],Actions.decrease(2));
+    expect(state).to.deep.equal([0,1,2]);
   });
 
 });
---
+++ test/jsx-demo.spec.js
@@ -0,0 +1,26 @@
+/* eslint-env mocha */
+import React from 'react';
+import chai, { expect } from 'chai';
+import TestUtils from 'react-addons-test-utils';
+import chaiJSX from 'chai-equal-jsx';
+chai.use(chaiJSX);
+
+const Comp = ({active, txt}) => {
+
+  if(active){
+    return <div className="act-class">{txt}</div> 
+  }  
+  return <div >{txt}</div>;
+};
+
+describe('Testing jsx', () => {
+  it('should be equals component', () => {
+
+    const renderer = TestUtils.createRenderer();
+    renderer.render(<Comp active={true} txt={'alex'} />);
+
+    let output = renderer.getRenderOutput();
+    expect(output.props.className).to.equal('act-class');
+    expect(output.props.children).to.equal('alex');
+  });
+});
